725a6e6b15dfdb7d75b3ff4806e8a5bbfc5a12dd
Dr. Dizz <bryansaltzman@gmail.com>
2024-04-02 11:15:03 -0700

    Update resizePreviewWindow.js

--- resizePreviewWindow.js
+++ resizePreviewWindow.js
@@ -4,18 +4,21 @@ window.onmessage = function(e){
         var sidebar_width = 320
         window.resizeTo(400, 753);
         window.moveTo(sidebar_width, 0);
+        console.log('mobile'  )
       } else if(e.data.size === 'desktop') {
         var sidebar_width = 320
         var win_width = screen.width - sidebar_width
         var win_height = screen.height
         window.resizeTo(win_width, win_height);
         window.moveTo(sidebar_width, 0);
+        console.log('desktop'  )
       } else if(e.data.size === 'fullscreen') {
         var sidebar_width = 40
         var win_width = screen.width - sidebar_width
         var win_height = screen.height
         window.resizeTo(win_width, win_height);
         window.moveTo(sidebar_width, 0);
+        console.log('fullscreen'  )
       }
     }
 };
